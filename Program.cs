f8aaf4e6918915293fd164b89b9f77ed21f3932e
doniyorniazov <niazovd@gmail.com>
2022-10-14 10:15:03 +0500

    test

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.AccessControl;
+using StackExchange.Redis;
+
+string connectionString = "connection string";
+
+var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+{
+    return ConnectionMultiplexer.Connect(connectionString);
+});
+
+IDatabase cache = lazyConnection.Value.GetDatabase();
+
+Console.WriteLine("Reading Cache : " + cache.StringGet("Session333"));
+Console.WriteLine("Writing Cache : " + cache.StringSet("Session333","Writing something to Redis" + DateTime.Now.ToShortTimeString()));
+Console.WriteLine("Reading Cache : " + cache.StringGet("Session333"));
+cache.KeyExpire("Session333", DateTime.Now);
+
+lazyConnection.Value.Dispose();
+
+Console.WriteLine("Done!");
+Console.ReadLine();
